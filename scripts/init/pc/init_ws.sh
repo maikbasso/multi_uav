46f7afe29c9101457f46314b6824f8001919dbce
Maik Basso <maik@maikbasso.com.br>
2020-02-06 12:42:56 -0300

    Fix a path in scripts.

--- scripts/init/pc/init_ws.sh
+++ scripts/init/pc/init_ws.sh
@@ -13,4 +13,4 @@ px4_dir=/home/$USER/drone_simulator_ws/multi_uav_dependencies/firmware/px4
 #export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$px4_dir/Tools/sitl_gazebo
 source $px4_dir/Tools/setup_gazebo.bash $px4_dir $px4_dir/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$px4_dir:$px4_dir/Tools/sitl_gazebo
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/home/maik/drone_simulator_ws/devel/lib
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/home/$USER/drone_simulator_ws/devel/lib
