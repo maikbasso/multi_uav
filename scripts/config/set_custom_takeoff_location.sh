8a23746d6530617d59aafed06f21a799f41edb9e
Maik Basso <maik@maikbasso.com.br>
2020-02-14 12:58:02 -0300

    Adding a script to change the GPS coordinates of takeoff location point.

---
+++ scripts/config/set_custom_takeoff_location.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# You should run this before starting gazebo simulation
+
+# Modify the starting point to CAAERO-RS (CLUBE ALVORADA DE AEROMODELISMO)
+export PX4_HOME_LAT=-29.994195
+export PX4_HOME_LON=-51.016244
+export PX4_HOME_ALT=28.5
